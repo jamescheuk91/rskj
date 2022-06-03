934c847c0f1c90c7f01a6b32f6a711135d00e4de
Heitor Machado <heitor.m@gmail.com>
2022-06-03 16:49:25 -0300

    ad tx to blocl pre mine on new pending tx

--- rskj-core/src/main/java/co/rsk/mine/MinerServerImpl.java
+++ rskj-core/src/main/java/co/rsk/mine/MinerServerImpl.java
@@ -379,9 +379,8 @@ public class MinerServerImpl implements MinerServer {
      */
     @Override
     public MinerWork getWork() {
-        MinerWork work = currentWork;
 
-        buildBlockToMine(false);
+        MinerWork work = currentWork;
         
         if (work.getNotify()) {
             /**
@@ -396,7 +395,7 @@ public class MinerServerImpl implements MinerServer {
                     return currentWork;
                 }
                 currentWork = new MinerWork(currentWork.getBlockHashForMergedMining(), currentWork.getTarget(),
-                        currentWork.getFeesPaidToMiner(), false, currentWork.getParentBlockHash());
+                       currentWork.getFeesPaidToMiner(), false, currentWork.getParentBlockHash());
             }
         }
         return work;
@@ -538,6 +537,8 @@ public class MinerServerImpl implements MinerServer {
                 return;
             }
 
+            logger.info("****> on Best Block !!!");
+            
             logger.trace("Start onBestBlock");
 
             logger.debug(
@@ -548,6 +549,19 @@ public class MinerServerImpl implements MinerServer {
             buildBlockToMine(false);
 
             logger.trace("End onBestBlock");
+        }
+        
+        @Override
+        public void onPendingTransactionsReceived(List<Transaction> transactions) {
+            
+            if (isSyncing()) {
+                return;
+            }
+            
+            logger.info("****> Pending Transactions Received !!!");
+            
+            buildBlockToMine(false);
+            
         }
 
         private boolean isSyncing() {
